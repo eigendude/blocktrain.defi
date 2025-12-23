4c52796b7737253b51b3f99840aaf131e34226de
Garrett Brown <eigendebugger@gmail.com>
2025-12-22 18:04:58 -0800

    Import npx create-onchain mini app

---
+++ minikit.config.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2025-2026 blocktrain.defi
+ * https://github.com/blocktrain/blocktrain-dapp
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See the file LICENSE.txt for more information.
+ */
+
+const ROOT_URL =
+  process.env.NEXT_PUBLIC_URL ||
+  (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) ||
+  "http://localhost:3000";
+
+/**
+ * MiniApp configuration object. Must follow the mini app manifest specification.
+ *
+ * @see {@link https://docs.base.org/mini-apps/features/manifest}
+ */
+export const minikitConfig = {
+  accountAssociation: {
+    header: "",
+    payload: "",
+    signature: "",
+  },
+  baseBuilder: {
+    ownerAddress: "",
+  },
+  miniapp: {
+    version: "1",
+    name: "blocktrain.defi",
+    subtitle: "",
+    description: "",
+    screenshotUrls: [],
+    iconUrl: `${ROOT_URL}/icon.png`,
+    splashImageUrl: `${ROOT_URL}/splash.png`,
+    splashBackgroundColor: "#000000",
+    homeUrl: ROOT_URL,
+    webhookUrl: `${ROOT_URL}/api/webhook`,
+    primaryCategory: "utility",
+    tags: ["example"],
+    heroImageUrl: `${ROOT_URL}/hero.png`,
+    tagline: "",
+    ogTitle: "",
+    ogDescription: "",
+    ogImageUrl: `${ROOT_URL}/hero.png`,
+  },
+} as const;
