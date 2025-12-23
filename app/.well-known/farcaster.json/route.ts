13ca73080911d0ca459682cfa8fc9a14920aeae3
Garrett Brown <eigendebugger@gmail.com>
2025-12-22 18:04:58 -0800

    Import npx create-onchain mini app

---
+++ app/.well-known/farcaster.json/route.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2025-2026 blocktrain.defi
+ * https://github.com/blocktrain/blocktrain-dapp
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See the file LICENSE.txt for more information.
+ */
+
+import { withValidManifest } from "@coinbase/onchainkit/minikit";
+
+import { minikitConfig } from "../../../minikit.config";
+
+export async function GET(): Promise<Response> {
+  return Response.json(withValidManifest(minikitConfig));
+}
---
+++ next.config.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2025-2026 blocktrain.defi
+ * https://github.com/blocktrain/blocktrain-dapp
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See the file LICENSE.txt for more information.
+ */
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+export default nextConfig;
